ddb27f9d569a9d766d7d46ed35f22950fd46f55a
Lukas Burger <lukas.burger@yale.edu>
2019-11-14 15:58:41 -0500

    esp receiver code

--- esp_mesh_receiver/esp_mesh_receiver.ino
+++ esp_mesh_receiver/esp_mesh_receiver.ino
@@ -8,32 +8,20 @@ painlessMesh mesh;
 #define   MESH_PASSWORD   "goodbyemark"
 #define   MESH_PORT       5555
 
-// User stub
-void sendMessage(); // Prototype so PlatformIO doesn't complain
-
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
-
-void sendMessage() {
-  String msg = "Hello It's Joe";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval(random( TASK_SECOND * 1, TASK_SECOND * 5 ));
-}
-
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  Serial.printf("%u,%s\n", from, msg.c_str());
 }
 
 void newConnectionCallback(uint32_t nodeId) {
-  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+//  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
 
 void changedConnectionCallback() {
-  Serial.printf("Changed connections\n");
+//  Serial.printf("Changed connections\n");
 }
 
 void nodeTimeAdjustedCallback(int32_t offset) {
-  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
+//  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
 }
 
 
@@ -46,9 +34,6 @@ void setup() {
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-
-  userScheduler.addTask( taskSendMessage );
-  taskSendMessage.enable();
 }
 
 void loop() {
