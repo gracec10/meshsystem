dddcbdae9f33fa58c030d686620581bbed8e4af4
joe-swensen <43071566+joe-swensen@users.noreply.github.com>
2019-11-15 14:00:33 -0600

    Update esp_mesh.ino

--- esp_mesh/esp_mesh.ino
+++ esp_mesh/esp_mesh.ino
@@ -1,5 +1,30 @@
 #include <painlessMesh.h>
 #include <AsyncTCP.h>
+#include <DHTesp.h>
+#include <Ultrasonic.h>
+
+
+/* The below section should be changed for each individual 
+ *  ESP32 depending on its configuration. If the ESP has the 
+ *  particular sensor as listed below, it should be replaced 
+ *  with the pin value of that sensor. Otherwise, set the 
+ *  value to 0 and it will be ignored. This is for consistent
+ *  formatting across messages sent and logged with the network.
+ */
+#define id 1 //Set the ID of the ESP32 within the context of the system
+#define HTS 0 //Set the HTS temp/humidity sensor pin. Set to 0 if this ESP does not have this sensor.
+#define HAL 0 //Set the Hall sensor pin. Set to 0 if this ESP does not have this sensor.
+#define LIT 0 //Set the photoresistor pin. Set to 0 if this ESP does not have this sensor.
+#define USN 0 //Set the ultrasonic distance sensor trigger pin. Set to 0 if this ESP does not have this sensor.
+#define USE 0 //Set the ultrasonic distance sensor echo pin. If the trigger pin is set to 0 this will be ignored.  Set to 0 if this ESP does not have this sensor.
+
+//Set up sensor variables 
+DHTesp dht;
+TaskHandle_t tempTaskHandle = NULL;
+Ultrasonic ultrasonic(USN, USE, 40000UL);
+int hallState = 0;
+int lightValue;
+String toSend;
 
 Scheduler userScheduler; // to control your personal task
 painlessMesh mesh;
@@ -14,10 +39,24 @@ void sendMessage(); // Prototype so PlatformIO doesn't complain
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
 void sendMessage() {
-  String msg = "Hello It's Joe";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval(random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+  toSend = "ESP" + String(id);
+  if(HTS != 0) { //Add temp / humidity values
+    TempAndHumidity lastValues = dht.getTempAndHumidity();
+    toSend = toSend.concat(",TMP" + String(lastValues.temperature,0));
+    toSend = toSend.concat(",HMD" + String(lastValues.humidity,0));
+  };
+  if(HAL !=0) { //Add hall sensor value
+    toSend = toSend.concat(",HAL" + String(digitalRead(HAL)));
+  };
+  if(LIT != 0) { //Add photoresistor value
+    toSend = toSend.concat(",LIT" + String(analogRead(LIT)));
+  };
+  if(USN != 0 && USE != 0) { //Add ultrasonic value
+    toSend = toSend.concat(",USN" + String(ultrasonic.read()));
+  };
+  toSend.concat("\r\n");
+  mesh.sendBroadcast(toSend);
+  taskSendMessage.setInterval(0.1);
 }
 
 void receivedCallback( uint32_t from, String &msg ) {
@@ -49,6 +88,12 @@ void setup() {
 
   userScheduler.addTask( taskSendMessage );
   taskSendMessage.enable();
+  if(HTS != 0) {
+    dht.setup(HTS, DHTesp::DHT11);
+  };
+  if(HAL != 0) {
+    pinMode(HAL,INPUT);
+  };
 }
 
 void loop() {
